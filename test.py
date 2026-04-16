41abb50b5b85637770d2636e15b7bfb138b763ea
benmwangi <7525481+benmwangi@users.noreply.github.com>
2026-04-16 18:33:38 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,31 @@
+dog_data = [['Name', 'Breed', 'Good Doggy?', 'Length [in]', 'Weight Range', 'Body Temp [F]'],
+["Buddy", "Golden Retriever", "Good", 24, 'Overweight', 101.5],
+["Cooper", "Doberman Pinscher", "Bad", 28, 'Normal',  105.2],
+["Max", "GERMAN Shepherd", "Good", 26, 'Normal', 100.2],
+["Bella", "Chow Chow", "Bad", 20, 'Normal', 100.5],
+["Lucy", " Yorkshire Terrier", "Good", 9, 'Normal', 102.0],
+["Charlie", "Beagle", "Good", 13, 'Overweight', 100.4],
+["Daisy", "Akita", "Bad", 24, 'Normal',99.8],
+["Bailey", "JACK russell terrier", "Bad", 10, 'Underweight', 101.2],
+["Lola", "Rottweiler   ", "Good", 27, 'Normal', 102.5],
+["Sadie", "Siberian_Husky", "Bad", 23, 'Normal', 99.5],
+["Penny", "STANDARD Poodle", "Good", 18, 'Underweight', 100.7],
+["Bruno", "Bulldog", "Bad", 14, 'Normal', 104.1],
+["Rosie", "Beagle", "Good", 13, 'Normal', 100.6],
+["Duke", "    DOBERMAN PINSCHER", "Good", 28,  'Normal',100.1],
+["Luna", "Shiba Inu", "Good", 17, 'Obese',  100.3],
+["Scout", "Golden_Retriever", "Good", 22, 'Normal', 103.5],
+["Rex", "Rottweiler", "Bad", 23, 'Normal', 100.2],
+]
+
+for dog in dog_data:
+    breed = dog[1]
+    cleaned_string = breed.strip().replace("_"," ").title()
+    dog_data.index(dog). = cleaned_string
+    # dog.replace(dog[1], cleaned_string)
+    print(dog_data)
+
+dog_data.
+word = "GERMAN Shepherd "
+word.
+# print(clean_string(word))
